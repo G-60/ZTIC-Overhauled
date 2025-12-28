e289cb078eeb03e803be0994befd57a7f5e86899
G-60 <131556546+G-60@users.noreply.github.com>
2025-12-28 17:00:32 +0300

    Modify App.tsx to use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,54 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-import Home from "./pages/Home.tsx"
-import Departments from './pages/departments/Departments.tsx'
-// Cybersecurity Pages
-import Cyber from './pages/departments/cybersecurity/About.tsx'
-import Members from './pages/departments/cybersecurity/Members.tsx'
-import Gplan from '@/pages/departments/cybersecurity/GuidePlan.tsx'
+import Home from "./pages/Home";
+import Departments from "./pages/departments/Departments";
 
+// Cybersecurity pages
+import Cyber from "./pages/departments/cybersecurity/About";
+import Members from "./pages/departments/cybersecurity/Members";
+import Gplan from "./pages/departments/cybersecurity/GuidePlan";
 
+import NotFound from "./pages/NotFound";
+import Gallery from "./pages/Gallery";
+import Portals from "./pages/Portals";
+import About from "./pages/about/About";
 
-
-
-
-
-
-import NotFound from './pages/NotFound.tsx' 
-import Gallery from './pages/Gallery.tsx'
-import Portals from "./pages/Portals.tsx"
-import About from './pages/about/About.tsx'
-
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
-    errorElement: <NotFound />
+    errorElement: <NotFound />,
   },
   {
-    path: '/Departments',
-    element: <Departments />
+    path: "/Departments",
+    element: <Departments />,
   },
   {
-    path: '/Gallery',
-    element: <Gallery />
+    path: "/Gallery",
+    element: <Gallery />,
   },
   {
-    path: '/Portals',
-    element: <Portals />
+    path: "/Portals",
+    element: <Portals />,
   },
   {
-    path: '/About',
-    element: <About />
-  },
-  // Cybersecurity
-  {
-    path: '/Departments/Cybersecurity/About',
-    element: <Cyber />
+    path: "/About",
+    element: <About />,
   },
   {
-    path: '/Departments/Cybersecurity/Members',
-    element: <Members />
+    path: "/Departments/Cybersecurity/About",
+    element: <Cyber />,
   },
   {
-    path: '/Departments/Cybersecurity/Guide-Plan',
-    element: <Gplan />
-  }
+    path: "/Departments/Cybersecurity/Members",
+    element: <Members />,
+  },
+  {
+    path: "/Departments/Cybersecurity/Guide-Plan",
+    element: <Gplan />,
+  },
 ]);
 
 export default function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
