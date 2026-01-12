c6a4dbb98eb2ffb3078c829f5f2d0c612e159d5b
G-60 <131556546+G-60@users.noreply.github.com>
2026-01-12 22:03:11 +0300

    Modifiy vite config for page load optimization

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,22 @@ import { defineConfig } from "vite"
 import { fileURLToPath } from "node:url"
 import { resolve } from "node:path"
 import tailwindcss from "@tailwindcss/vite"
+
 const __dirname = fileURLToPath(new URL(".", import.meta.url))
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ZTIC-Overhauled"
-,
+  base: "/ZTIC-Overhauled",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          // add other big libs if needed
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
